213c06379246301b21b46b5634ce19b93e5966eb
Anton Polovoy <admin@Admins-MacBook-Pro.local>
2023-11-29 10:46:02 +0300

    SnapKit setup

--- Twinky/ViewControllers/SettingsViewController/SettingsViewController.swift
+++ Twinky/ViewControllers/SettingsViewController/SettingsViewController.swift
@@ -7,11 +7,24 @@
 
 import Foundation
 import UIKit
-
+import SnapKit
 class SettingsViewController: UIViewController {
+    
+    private lazy var label: UILabel = {
+        let lbl = UILabel()
+        lbl.text = "hdifhdi"
+        lbl.textColor = .titleColor
+        lbl.font = .sourceSans(ofSize: 25, style: .blackItalic)
+        return lbl
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("SASI")
+        view.addSubview(label)
+        view.backgroundColor = .backgroundViewColor
+        
+        label.snp.makeConstraints {
+            $0.center.equalTo(view.center)
+        }
     }
 }
